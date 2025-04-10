2cd03e8ae5fe7e561619dc90548f159f3bb22565
PedroLucenaAlves <alvespedroalonso@gmail.com>
2025-04-10 18:33:39 -0300

    Criando nosso read do Crud com a criação de um get no controller, um caso de uso para localizar um id, nosso pacote de response do endereço e do customer e nossa porta de entrada para utilizar nossa localização por id do nosso customer.

---
+++ src/main/java/br/com/hexagonal/hexagonal/adapters/in/controller/response/CustomerResponse.java
@@ -0,0 +1,17 @@
+package br.com.hexagonal.hexagonal.adapters.in.controller.response;
+
+import lombok.Data;
+
+@Data
+public class CustomerResponse {
+
+    private String name;
+
+    private AddressResponse address;
+
+    private String cpf;
+
+    private Boolean isValidCPF;
+
+
+}
---
+++ src/main/java/br/com/hexagonal/hexagonal/adapters/out/FindCustomerByIdAdapter.java
@@ -0,0 +1,27 @@
+package br.com.hexagonal.hexagonal.adapters.out;
+
+import br.com.hexagonal.hexagonal.adapters.out.repository.CustomerRepository;
+import br.com.hexagonal.hexagonal.adapters.out.repository.mapper.CustomerEntityMapper;
+import br.com.hexagonal.hexagonal.application.core.domain.Customer;
+import br.com.hexagonal.hexagonal.application.ports.out.FindCustomerByIdOutputPort;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class FindCustomerByIdAdapter  implements FindCustomerByIdOutputPort {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private CustomerEntityMapper customerEntityMapper;
+
+    @Override
+    public Optional<Customer> find(String id) {
+        var customerEntity = customerRepository.findById(id);
+        //sempre ira retornar um optional de customer
+        return customerEntity.map(entity -> customerEntityMapper.toCustomer(entity)); //map usado no optional para transformar objeto
+    }
+}
