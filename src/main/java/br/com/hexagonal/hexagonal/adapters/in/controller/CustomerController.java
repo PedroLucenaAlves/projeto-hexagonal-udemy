0ca9ce057dd3fcb59d57c478ec2e01e7a95d1af9
PedroLucenaAlves <alvespedroalonso@gmail.com>
2025-04-10 19:03:24 -0300

    Criando nosso update do Crud.

--- src/main/java/br/com/hexagonal/hexagonal/adapters/in/controller/CustomerController.java
+++ src/main/java/br/com/hexagonal/hexagonal/adapters/in/controller/CustomerController.java
@@ -3,12 +3,16 @@ package br.com.hexagonal.hexagonal.adapters.in.controller;
 import br.com.hexagonal.hexagonal.adapters.in.controller.mapper.CustomerMapper;
 import br.com.hexagonal.hexagonal.adapters.in.controller.request.CustomerRequest;
 import br.com.hexagonal.hexagonal.adapters.in.controller.response.CustomerResponse;
+import br.com.hexagonal.hexagonal.application.core.domain.Customer;
 import br.com.hexagonal.hexagonal.application.ports.in.FindCustomerByIdInputPort;
 import br.com.hexagonal.hexagonal.application.ports.in.InsertCustomerInputPort;
+import br.com.hexagonal.hexagonal.application.ports.in.UpdateCustomerInputPort;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.lang.annotation.Repeatable;
 
 @RestController
 @RequestMapping("/api/v1/customers")
@@ -23,6 +27,9 @@ public class CustomerController {
     @Autowired
     private CustomerMapper customerMapper;
 
+    @Autowired
+    private UpdateCustomerInputPort updateCustomerInputPort;
+
     @PostMapping
     public ResponseEntity<Void> insert(@Valid @RequestBody CustomerRequest customerRequest){
         var customer = customerMapper.toCustomer(customerRequest);
@@ -36,5 +43,14 @@ public class CustomerController {
         var customerResponse = customerMapper.toCustomerResponse(customer);
         return ResponseEntity.ok().body(customerResponse);
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> update(@PathVariable final String id, @RequestBody CustomerRequest customerRequest){
+        Customer customer = customerMapper.toCustomer(customerRequest);
+        customer.setId(id);
+        updateCustomerInputPort.update(customer, customerRequest.getZipCode());
+        return ResponseEntity.noContent().build();
+    }
+
 
 }
